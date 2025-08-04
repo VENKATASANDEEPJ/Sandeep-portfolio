69e36e52e8fc8171a73b05bcd76ad1fa95389fcf
VENKATASANDEEPJ <venkatasandeepj0987@gmail.com>
2025-08-04 20:15:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-document.getElementById("showProfile").addEventListener("click", () => {
-  document.getElementById("profileSection").classList.remove("hidden");
+document.addEventListener("DOMContentLoaded", function () {
+  const emailBtn = document.getElementById("emailBtn");
+  const detailsCard = document.getElementById("detailsCard");
+
+  emailBtn.addEventListener("click", () => {
+    // Toggle visibility
+    if (detailsCard.style.display === "block") {
+      detailsCard.style.display = "none";
+    } else {
+      detailsCard.style.display = "block";
+    }
+  });
 });
