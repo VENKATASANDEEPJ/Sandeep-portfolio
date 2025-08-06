bbf12c59da1adb150c382edd7b8e980ca83b7ae7
VENKATASANDEEPJ <venkatasandeepj0987@gmail.com>
2025-08-06 21:30:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // Handle Welcome Screen Button
-const enterBtn = document.getElementById('enterBtn');
-const welcomeScreen = document.getElementById('welcomeScreen');
-const portfolio = document.getElementById('portfolio');
+document.addEventListener("DOMContentLoaded", () => {
+  const enterBtn = document.getElementById("enterBtn");
+  const welcomeScreen = document.getElementById("welcomeScreen");
+  const portfolio = document.getElementById("portfolio");
 
-enterBtn.addEventListener('click', () => {
-  welcomeScreen.style.display = 'none';
-  portfolio.classList.remove('hidden');
-  portfolio.classList.add('show');
+  enterBtn.addEventListener("click", () => {
+    welcomeScreen.style.display = "none";
+    portfolio.classList.remove("hidden");
+    portfolio.classList.add("show");
+  });
 });
